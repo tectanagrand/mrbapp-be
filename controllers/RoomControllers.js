627b11108de8302ceaeb245d8f60a08c04e9a621
tectanagrand <rtektano@gmail.com>
2024-02-22 11:05:47 +0700

    feat : get all data room with facility

--- controllers/RoomControllers.js
+++ controllers/RoomControllers.js
@@ -16,6 +16,34 @@ const RoomControllers = {
       client.release();
     }
   },
+  getAllRoomWithFac: async (req, res) => {
+    const Client = new DbConn();
+    const client = await Client.initConnection();
+    try {
+      const getroom = await client.query("SELECT * from mst_room");
+      const rooms = getroom[0];
+      let roomFac = [];
+      let promise = [];
+      rooms.forEach((item) => {
+        promise.push(
+          client.query(`SELECT MF.nama from fas_room FR LEFT JOIN mst_fas MF
+        ON fr.id_fasilitas = MF.id_fasilitas 
+        WHERE id_ruangan = '${item.id_ruangan}'`)
+        );
+      });
+      const dataGet = await Promise.all(promise);
+      rooms.forEach((item, index) => {
+        const fac = dataGet[index][0].map((item) => item.nam);
+        roomFac.push({ ...item, fasilitas: fac });
+      });
+      res.status(200).send({ data: roomFac });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: error.message });
+    } finally {
+      client.release();
+    }
+  },
 };
 
 module.exports = RoomControllers;
--- routes/Room.js
+++ routes/Room.js
@@ -3,5 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", RoomController.getAllRoom);
+router.get("/fas", RoomController.getAllRoomWithFac);
 
 module.exports = router;
