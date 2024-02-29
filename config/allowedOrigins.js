87e78bb69f39d706f4860ff846cebe062bdd3a21
tectanagrand <rtektano@gmail.com>
2024-02-29 10:20:13 +0700

    add : allowed origin dev

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const origins = ["https://localhost:3000", "http://localhost:3000"];
+const origins = [
+  "https://localhost:3000",
+  "http://localhost:3000",
+  "https://186b-49-156-23-37.ngrok-free.app",
+];
 
 module.exports = origins;
